75d08dcafe6893bddf6cbe907b71d46d2e84a653
yuguo2912 <arnaudeaudarsonval@gmail.com>
2025-06-25 15:23:55 +0200

    fix run

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose.compiler)
 
 
 }
@@ -42,7 +43,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "2.0.0"
     }
     packaging {
         resources {
--- app/src/main/java/com/esgi/securivault/networking/RetrofitHttpClient.kt
+++ app/src/main/java/com/esgi/securivault/networking/RetrofitHttpClient.kt
@@ -7,8 +7,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHttpClient{
     private const val BASE_URL = "http://localhost:8080/"
-    private const val REGISTER_URL = BASE_URL+ "auth"
-    private const val LOGIN_URL = REGISTER_URL + "login"
+    private const val REGISTER_URL = BASE_URL+ "auth/"
+    private const val LOGIN_URL = REGISTER_URL + "login/"
 
 
     val loginRetrofit: Retrofit by lazy{
